f2809ca0d5e3229796497b36f9a26a0609fb71d8
Lashawn academy <160122359+mmoya2024@users.noreply.github.com>
2026-09-24 23:36:20 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE = 'lashawn-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './assets/css/style.css',
+  './assets/js/data.js',
+  './assets/js/pages.js',
+  './assets/js/app.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request).catch(() => caches.match('./index.html')))
+  );
+});
